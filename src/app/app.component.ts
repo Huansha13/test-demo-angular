60475c04bdbd64f349fcb0f2f5edfd307f64e83b
lilab03 <julio.ramirez@lilab.pe>
2024-09-15 13:00:01 -0500

    feat: http get lista productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+export interface Producto {
+  price: string
+  name: string
+  id: string
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo-front';
+  lisproductos: Producto[] = [];
+  private  url = 'http://localhost:8080/demo/api/v1/products'
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Producto[]>(this.url).subscribe((data) => {
+      this.lisproductos = data;
+    }
+    );
+  }
 }
